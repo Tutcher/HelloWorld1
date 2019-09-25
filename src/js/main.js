5cd55866254d0881b223c5c6b5d2edb1a226bbe4
Tutcher <anna.tutcher@mail.ru>
2019-09-25 20:04:06 +0300

    partJS

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,22 @@
 var img = document.getElementById('img');
 document.getElementById('my_form').addEventListener('submit', function(e) {
   img.style.display = 'block';
-  img.style.top = (document.documentElement.clientHeight - img.offsetHeight) / 2 + 'px';
-  img.style.left = (document.documentElement.clientWidth - img.offsetWidth) / 2 + 'px';
+  img.style.top = (document.documentElement.clientHeight - img.offsetHeight) / 5 + 'px';
+  img.style.left = (document.documentElement.clientWidth - img.offsetWidth) / 12 + 'px';
   e.preventDefault();
 });
 document.addEventListener('click', function(e) {
   var target = e.target;
   if(target == img.firstElementChild || target == img) return;
   img.style.display = 'none';
+});
+var Img = document.getElementById('img_footer');
+document.getElementById('form_footer').addEventListener('submit', function(e) {
+  Img.style.display = 'block';
+  e.preventDefault();
+});
+document.addEventListener('click', function(e) {
+  var target = e.target;
+  if(target == Img.firstElementChild || target == Img) return;
+  Img.style.display = 'none';
 });
